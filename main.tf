09557853c1a1645c61b36ef26cd0bc0abeb04c78
andresb39 <andresb39@gmail.com>
2023-01-30 11:53:56 -0500

    Test: bucket

--- main.tf
+++ main.tf
@@ -3,16 +3,6 @@ resource "aws_s3_bucket" "my_test_bucket" {
   bucket = "my_test_bucket_checkov"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "my_test_bucket" {
-  bucket = aws_s3_bucket.my_test_bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket_acl" "temp_files_api_acl" {
   bucket = aws_s3_bucket.my_test_bucket.id
   acl    = "private"
