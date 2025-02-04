d62f294aea7ee9241d729cbf0684016bf3b77a8b
Jesus Andres Bergaño G. <andresb39@gmail.com>
2025-02-04 17:02:58 -0500

    ✨ Update S3 bucket application tag to "my_test_app6" for clarity and consistency in resource identification.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "my_test_bucket" {
   bucket = var.bucket_name
   tags = {
     Name        = var.bucket_name
-    Application = "my_test_app5"
+    Application = "my_test_app6"
   }
 }
 
